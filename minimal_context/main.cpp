dc157ceb83ea66b1a5b56e8fc1b1b1e314dbca5c
Mateusz Borycki <mateuszborycki@gmail.com>
2017-12-17 23:53:45 +0100

    Add keyboard support.

--- minimal_context/main.cpp
+++ minimal_context/main.cpp
@@ -18,9 +18,41 @@ void consoleLog(const char *str)
     jsLog(str, size);
 }
 
-extern "C" int doubler(int x)
+namespace
 {
-    jsFillRect(0,0,100,100);
-    consoleLog("Hello world");
-    return 2 * x;
+    bool keys[256] = {0};
+}
+
+//exports
+extern "C"
+{
+    void runCallback(void (*callback)()) { callback(); }
+
+    void onKey(int key, bool pressed)
+    {
+        if (key > 255) return;
+        keys[key] = pressed;
+    }
+
+    void onFrame(float dt)
+    {
+
+        static float posX = 0.0f, posY = 0.0f;
+        jsClearRect(posX,posY,100,100);
+
+        float speed = 100.0f;
+        if(keys[37]) posX -= speed * dt;
+        if(keys[39]) posX += speed * dt;
+
+        if(keys[38]) posY -= speed * dt;
+        if(keys[40]) posY += speed * dt;
+
+        jsFillRect(posX,posY,100,100);
+    }
+
+    int main()
+    {
+        consoleLog("Hello world");
+        return 0;
+    }
 }
